bc1da7dd0e985d50881166e498f953e70edc33ce
Danil <gabitov1983@gmail.com>
2020-12-09 01:10:45 +0300

    try add dotenv

--- app/views.py
+++ app/views.py
@@ -5,12 +5,17 @@ from flask_mail import Mail, Message
 import random
 import os
 import time
+from dotenv import load_dotenv
 
 from werkzeug.utils import redirect
 
 from app.dbrout import pgdb
 from app.imgbb import imgrout
 from app.sessions import sessions
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 app.secret_key = os.environ['APP_SECRET_KEY']
